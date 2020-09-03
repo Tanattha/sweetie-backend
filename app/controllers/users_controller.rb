3c2e439ee04ee77d375f257e77b634b1676b5724
Tanattha <tanattha.thuenim@gmail.com>
2020-09-02 21:37:49 -0400

    change link footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,15 @@ class UsersController < ApplicationController
           user = User.create(name: params[:name], email: params[:email])
           user.carts.build(checkout: true, user_id: user.id, total: params[:total])
           user.save
-          if user.save 
-            
-            binding.pry
-            
+          if user.save        
             get_cartItem = params[:cartItems]
-            get_cartItem.each do |cart|
-              CartProduct.create(cart_id: user.carts.ids, product_id: params[:product_id])
+            get_cartId = user.carts.first.id
+            get_cartItem.each do |cartItem|
+              CartProduct.create(cart_id: get_cartId, product_id: cartItem.values.first)
+              puts "cart_id:  #{get_cartId}"
+              puts "product_id:  #{cartItem.values.first}"
             end
-            render json: user
+            render json: get_cartItem
           else
             render json: {message: "Something went wrong. Please make sure all fields are entered correctly."}
           end
